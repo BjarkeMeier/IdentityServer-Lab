027e8fd2dba8d5df7522ba5f8bf17d3e954f4f2c
Bjarke Meier <bm@itu.dk>
2017-06-21 22:02:32 +0200

    Initial quickstart

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 
-namespace WebApplication1
+namespace IdentityServer
 {
     public class Program
     {
@@ -13,6 +13,7 @@ namespace WebApplication1
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:5000/")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
